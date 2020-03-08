f981f66d88706d780f5a91c597bffafd66cefcc6
jonty <501211312@qq.com>
2020-03-08 20:01:17 +0800

    添加项目文件。

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRChat.Hubs
+{
+    public class ChatHub:Hub
+    {
+        //hub类管理连接、组和消息
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
---
+++ wwwroot/js/chat.js
@@ -0,0 +1,30 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+//Disable send button until connection is established
+document.getElementById("sendButton").disabled = true;
+
+//创建并且启动连接
+connection.on("ReceiveMessage", function (user, message) {
+	var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+	var encodedMsg = user + " says " + msg;
+	var li = document.createElement("li");
+	li.textContent = encodedMsg;
+	document.getElementById("messagesList").appendChild(li);
+});
+
+connection.start().then(function () {
+	document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+	return console.error(err.toString());
+});
+//向提交按钮添加一个用于向中心发送消息的处理程序
+document.getElementById("sendButton").addEventListener("click", function (event) {
+	var user = document.getElementById("userInput").value;
+	var message = document.getElementById("messageInput").value;
+	connection.invoke("SendMessage", user, message).catch(function (err) {
+		return console.error(err.toString());
+	});
+	event.preventDefault();
+});
